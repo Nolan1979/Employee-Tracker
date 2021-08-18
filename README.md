4ebd0f499e9cee086e7cf208506cbf0d1eaff7be
Josh Nolan <nolanjosh1979@gmail.com>
2021-08-18 14:31:43 -0500

    added README to repo

---
+++ README.md
@@ -0,0 +1,29 @@
+
+# Employee-Tracker
+
+## Description
+This is a an app that allows you to keep a database of your employees. It runs on node and Mysql.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Install node.js and Mysql to your machine.
+
+## Usage
+Copy this repo to your machine and clone it in your terminal. Run "node server" to start the app.
+
+## License
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+## Contributing
+You can clone this repo here https://github.com/Nolan1979/Employee-Tracker
+
+
+## Questions
+Having an issue? Email me with any questions about the app nolanjosh1979@gmil.com
--- server.js
+++ server.js
@@ -22,14 +22,14 @@ runIt = () => {
             message: 'What would you like to do?',
             name: 'options',
             choices: [
-                { name: ' View All Employees', value: 'VIEW_EMPLOYEES' },
+                { name: 'View All Employees', value: 'VIEW_EMPLOYEES' },
                 { name: 'View All Departments', value: 'VIEW_EMPLOYEES_BY_DEPT' },
                 { name: 'View All Roles', value: 'VIEW_EMPLOYEES_BY_ROLE' },
                 { name: 'Add Employee', value: 'ADD_NEW_EMPLOYEE' },
                 { name: 'Add Role', value: 'ADD_NEW_ROLE' },
                 { name: 'Add Department', value: 'ADD_NEW_DEPARTMENT' },
-
-                // 'Update Employee Role',
+                { name: 'Update Employee Role', value: 'UPDATE_EMPLOYEE_ROLE' },
+                { name: 'Exit', value: 'EXIT' }
             ]
         })
         .then(res => {
@@ -54,29 +54,53 @@ runIt = () => {
                 case 'ADD_NEW_DEPARTMENT':
                     addDepartment();
                     break;
-            }
-
+                case 'UPDATE_EMPLOYEE_ROLE':
+                    updateEmployeeRole();
+                    break;
+                case 'EXIT':
+                    connection.end();
+                    console.log('Enter "node server" to restart')
+            };
         })
 };
 
 
 function viewEmployees() {
 
-    connection.query('SELECT * FROM employee', function (err, res) {
+    connection.query(`SELECT employee.id AS employee_id, 
+    employee.first_name, 
+    employee.last_name, 
+    role.title, 
+    department.name, 
+    role.salary, 
+    CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON department.id = role.department_id
+    LEFT JOIN employee manager ON manager.id = employee.manager_id`, function (err, res) {
+        console.log('=======================')
         console.table(res)
     })
+    console.log('=======================')
+    runIt();
 };
 
 function viewEmployeesByDept() {
     connection.query('SELECT * FROM department', function (err, res) {
+        console.log('=======================')
         console.table(res)
     })
+    console.log('=======================')
+    runIt();
 };
 
 function viewEmployeesByRole() {
     connection.query('SELECT * FROM role', function (err, res) {
+        console.log('=======================')
         console.table(res)
     })
+    console.log('=======================')
+    runIt();
 };
 
 function addEmployee() {
@@ -105,11 +129,11 @@ function addEmployee() {
         .then(function (a) {
             const query = 'INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)'
             connection.query(query, [a.firstName, a.lastName, 1, 1], function (err, results) {
-                console.log(results); // results contains rows returned by server
+                console.log('================')
             });
+            runIt();
         });
 };
-
 
 function addRole() {
     inquirer.prompt([
@@ -132,8 +156,11 @@ function addRole() {
         .then(function (b) {
             const query = 'INSERT INTO role(title, salary, department_id) VALUES (?,?,?)'
             connection.query(query, [b.roleName, b.roleSalary, b.departmentId], function (err, results) {
-                console.log(results); // results contains rows returned by server
+                console.log('================')
+                console.log(results);
             });
+            console.log('=======================')
+            runIt();
         });
 };
 
@@ -147,10 +174,52 @@ function addDepartment() {
         .then(function (c) {
             const query = 'INSERT INTO department(name) VALUES (?)'
             connection.query(query, [c.departmentName], function (err, results) {
-                console.log(results); // results contains rows returned by server
+                console.log('================')
+                console.log('Added New Department');
+                console.log(results);
             });
+            console.log('=======================')
+            runIt();
         });
 };
 
+function updateEmployeeRole() {
+    inquirer.prompt([
+
+        {
+            type: 'list',
+            message: "Select employee",
+            name: 'employeeList',
+            choices: [
+                "Bob Uecker",
+                "Lynn Belvedere",
+                "Terry Jones",
+                "Terry Gilliam",
+                "Julius Erving",
+                "Pete Maravich",
+            ]
+        },
+        {
+            type: 'list',
+            message: "Select new role",
+            name: 'roleList',
+            choices: [
+                "Senior Accountant",
+                "Accountant",
+                "Senior Market Research Manager",
+                "Data Analyst",
+                "Sales Rep",
+                "Cashier",
+            ]
+        }])
+        .then(function (u) {
+            const query = 'UPDATE employee SET role_id AS role.title = roleList WHERE id = employeeList`'
+            connection.query(query, [u.empList, u.roleList], function (err, results) {
+                console.log('================')
+                console.log(results);
+            });
+            runIt();
+        });
+};
 
 runIt();

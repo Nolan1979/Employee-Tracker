0b47c566fa769adbfef8c6086c4077bcc3747ec8
Josh Nolan <nolanjosh1979@gmail.com>
2021-08-17 23:49:20 -0500

    add employee and add role functions written to server.js file

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { prompt } = require('inquirer');
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require('console.table');
@@ -24,11 +25,11 @@ runIt = () => {
                 { name: ' View All Employees', value: 'VIEW_EMPLOYEES' },
                 { name: 'View All Departments', value: 'VIEW_EMPLOYEES_BY_DEPT' },
                 { name: 'View All Roles', value: 'VIEW_EMPLOYEES_BY_ROLE' },
-                { name: 'Add Employee', value: 'ADD_NEW_EMPLOYEE' }
+                { name: 'Add Employee', value: 'ADD_NEW_EMPLOYEE' },
+                { name: 'Add Role', value: 'ADD_NEW_ROLE' },
 
-                // 'Add Employee',
                 // 'Update Employee Role',
-                // 'Add Role',
+                
                 // 'Add Department'
             ]
         })
@@ -45,8 +46,11 @@ runIt = () => {
                 case 'VIEW_EMPLOYEES_BY_ROLE':
                     viewEmployeesByRole();
                     break;
-                    case 'ADD_NEW_EMPLOYEE':
+                case 'ADD_NEW_EMPLOYEE':
                     addEmployee();
+                    break;
+                case 'ADD_NEW_ROLE':
+                    addRole();
                     break;
             }
 
@@ -68,50 +72,82 @@ function viewEmployeesByDept() {
 
 };
 
-    function viewEmployeesByRole() {
-        connection.query('SELECT * FROM role', function (err, res) {
-            console.table(res)
-        })
-    };
+function viewEmployeesByRole() {
+    connection.query('SELECT * FROM role', function (err, res) {
+        console.table(res)
+    })
+};
 
-    function addEmployee() {
-        inquirer.prompt([
+function addEmployee() {
+    inquirer.prompt([
 
-            {
-                type: 'input',
-                message: "What is new employee first's name?",
-                name: 'firstName',
-            },
-            {
-                type: 'input',
-                message: "What is new employee's last name?",
-                name: 'lastName',
-            },
-            {
-                type: 'input',
-                message: "What is new employee's role?",
-                name: 'roleName',
-            },
-            {
-                type: 'input',
-                message: "What is new employee's manager name?",
-                name: 'managerName',
-            }]);
-            
-            
-        // connection.query('SELECT * FROM employee', function (err, res) {
-        //     console.table(res)
-        };
-    
-
-    
-
+        {
+            type: 'input',
+            message: "What is new employee first's name?",
+            name: 'firstName',
+        },
+        {
+            type: 'input',
+            message: "What is new employee's last name?",
+            name: 'lastName',
+        },
+        {
+            type: 'input',
+            message: "What is new employee's role?",
+            name: 'roleName',
+        },
+        {
+            type: 'input',
+            message: "What is new employee manager's name?",
+            name: 'managerName',
+        }])
+        .then(function (a) {
+            const query = 'INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)'
+            connection.query(query, [a.firstName, a.lastName, 1,1], function (err, results) {
+                console.log(results); // results contains rows returned by server
+            });
+        });
+};
 
 
+function addRole() {
+    inquirer.prompt([
 
-    
+        {
+            type: 'input',
+            message: "What is new role name?",
+            name: 'roleName',
+        },
+        {
+            type: 'input',
+            message: "What is new role salary?",
+            name: 'roleSalary',
+        },
+        {
+            type: 'input',
+            message: "What is new role department?",
+            name: 'departmentId',
+        }])
+        .then(function (b) {
+            const query = 'INSERT INTO role(title, salary, department_id) VALUES (?,?,?)'
+            connection.query(query, [b.roleName, b.roleSalary, b.departmentId], function (err, results) {
+                console.log(results); // results contains rows returned by server
+            });
+        });
+};
 
-    runIt();
+
+
+
+
+
+
+
+
+
+
+
+runIt();
 
         // .then((data) => {
 
